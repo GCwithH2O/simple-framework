b62b59a55d88c41d5b89ae4f27901b4763045c0e
gc <gc@DESKTOP-D4P06I7>
2019-12-30 14:45:14 +0800

    redis工具类直接提供redistemplate对象，剩下自由操作

--- spurs-framework-core/src/main/java/com/spurs/framework/core/cache/Cache.java
+++ spurs-framework-core/src/main/java/com/spurs/framework/core/cache/Cache.java
@@ -5,59 +5,59 @@ import java.util.Map;
 
 public interface Cache {
 
-	public static final String CACHE_TYPE_EHCACHE = "ehcache";
-	public static final String CACHE_TYPE_REDIS = "redis";
-    /**
-     * 可超时的缓存
-     */
-    public static final String CACHE_NAME = "paladinCache";
-    /**
-     * 默认缓存名
-     */
-    public static final String CACHE_NAME_ETERNAL = "defaultCacheName";
-    /**
-     * 登录令牌缓存
-     */
-    public static final String CACHE_NAME_LOGIN_TOKEN = "loginTokenCacheName";
-    /**
-     * 短信验证码缓存
-     */
-    public static final String CACHE_NAME_SMS_CAPTCHA = "captchaCacheName";
-
-    /**
-     * 获取cacheName中key的值
-     * 
-     * @param cacheName
-     * @param key
-     * @return
-     */
-    public Object get(String cacheName, String key);
-
-    /**
-     * 存储缓存
-     * 
-     * @param cacheName
-     * @param key
-     * @param value
-     * @return
-     */
-    public boolean put(String cacheName, String key, Object value, int... cacheSeconds);
-
-    /**
-     * 清除指定key缓存
-     * 
-     * @param cacheName
-     * @param cacheKey
-     * @return
-     */
-    public boolean remove(String cacheName, String cacheKey);
-
-    /**
-     * 获取所有keys
-     * 
-     * @return
-     */
-    public List<Map<String, Object>> getAllDatas();
-
-    public boolean clear(String name);
+//	public static final String CACHE_TYPE_EHCACHE = "ehcache";
+//	public static final String CACHE_TYPE_REDIS = "redis";
+//    /**
+//     * 可超时的缓存
+//     */
+//    public static final String CACHE_NAME = "paladinCache";
+//    /**
+//     * 默认缓存名
+//     */
+//    public static final String CACHE_NAME_ETERNAL = "defaultCacheName";
+//    /**
+//     * 登录令牌缓存
+//     */
+//    public static final String CACHE_NAME_LOGIN_TOKEN = "loginTokenCacheName";
+//    /**
+//     * 短信验证码缓存
+//     */
+//    public static final String CACHE_NAME_SMS_CAPTCHA = "captchaCacheName";
+//
+//    /**
+//     * 获取cacheName中key的值
+//     * 
+//     * @param cacheName
+//     * @param key
+//     * @return
+//     */
+//    public Object get(String cacheName, String key);
+//
+//    /**
+//     * 存储缓存
+//     * 
+//     * @param cacheName
+//     * @param key
+//     * @param value
+//     * @return
+//     */
+//    public boolean put(String cacheName, String key, Object value, int... cacheSeconds);
+//
+//    /**
+//     * 清除指定key缓存
+//     * 
+//     * @param cacheName
+//     * @param cacheKey
+//     * @return
+//     */
+//    public boolean remove(String cacheName, String cacheKey);
+//
+//    /**
+//     * 获取所有keys
+//     * 
+//     * @return
+//     */
+//    public List<Map<String, Object>> getAllDatas();
+//
+//    public boolean clear(String name);
 }
--- spurs-framework-core/src/main/java/com/spurs/framework/core/cache/redis/RedisUtil.java
+++ spurs-framework-core/src/main/java/com/spurs/framework/core/cache/redis/RedisUtil.java
@@ -5,43 +5,56 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RedisUtil implements com.spurs.framework.core.cache.Cache {
 
-	@Resource(name = "redisCacheManager")
-	private RedisCacheManager cacheManager;
+//	@Resource(name = "redisCacheManager")
+//	private RedisCacheManager cacheManager;
+	@Autowired
+    private StringRedisTemplate redisTemplate;
+
+	public StringRedisTemplate getRedisTemplate() {
+		return redisTemplate;
+	}
+
+	public void setRedisTemplate(StringRedisTemplate redisTemplate) {
+		this.redisTemplate = redisTemplate;
+	}
 	
-	@Override
-	public Object get(String cacheName, String key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean put(String cacheName, String key, Object value, int... cacheSeconds) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean remove(String cacheName, String cacheKey) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public List<Map<String, Object>> getAllDatas() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean clear(String name) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+//	@Override
+//	public Object get(String cacheName, String key) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public boolean put(String cacheName, String key, Object value, int... cacheSeconds) {
+//		// TODO Auto-generated method stub
+////		redisTemplate.opsForValue().set("123", "sadad");
+//		return false;
+//	}
+//
+//	@Override
+//	public boolean remove(String cacheName, String cacheKey) {
+//		// TODO Auto-generated method stub
+//		return false;
+//	}
+//
+//	@Override
+//	public List<Map<String, Object>> getAllDatas() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public boolean clear(String name) {
+//		// TODO Auto-generated method stub
+//		return false;
+//	}
 
 }
